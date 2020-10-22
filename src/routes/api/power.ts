e8836344288cb206c450e314565b116e63a3c785
Francesco Compagnoni <compfrancesco@gmail.com>
2020-10-22 13:08:48 +0100

    Fix API route URLs

--- src/routes/api/power.ts
+++ src/routes/api/power.ts
@@ -3,20 +3,20 @@ import { setPower, getPower } from "../../hardware"
 
 const route : Array<Route> = [{
 	method: Method.GET,
-	url: "/power",
+	url: "/api/power",
 	handler: (request: any, response) => {
 		response.send({ok: true, result: { power: getPower() }});
 	}
 }, {
 	method: Method.PUT,
-	url: "/power",
+	url: "/api/power",
 	handler: (request: any, response) => {
 		setPower(true);
 		response.send({ok: true});
 	}
 }, {
 	method: Method.DELETE,
-	url: "/power",
+	url: "/api/power",
 	handler: (request: any, response) => {
 		setPower(false);
 		response.send({ok: true});
